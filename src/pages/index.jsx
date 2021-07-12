7f7caf514f52b5321409a304a816aaa119f0f0a8
Meenakshi Dhanani <meena24dhanani@gmail.com>
2021-07-12 15:30:49 +0530

    Adjust cards to add Postman Space Camp on Learning Center Home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -109,12 +109,12 @@ class IndexPage extends React.Component {
             </div>
             <div className="row justify-content-center">
               <div className="col-md-6 col-lg-4 mb-3 mb-md-4">
-                <SecondaryCard
-                  title="Community"
-                  description="Connect to the Postman community on our forum."
-                  cta="Visit Forum"
-                  ctaLink="https://community.postman.com/"
-                />
+              <SecondaryCard
+              title="Postman Space Camp"
+              description="See upcoming educational webinars."
+              cta="See Webinars"
+              ctaLink="https://www.postman.com/events/postman-space-camp/"
+            />
               </div>
               <div className="col-md-6 col-lg-4 mb-3 mb-md-4">
                 <SecondaryCard
@@ -274,11 +274,11 @@ class IndexPage extends React.Component {
           </div>
           <div className="col-md-6 col-lg-4 mb-3 mb-md-4">
             <SecondaryCard
-              title="Product roadmap"
-              description="Find out what we're working on."
-              cta="See the roadmap"
-              ctaLink="https://trello.com/b/4N7PnHAz/postman-roadmap-for-developers"
-            />
+                  title="Community"
+                  description="Connect to the Postman community on our forum."
+                  cta="Visit Forum"
+                  ctaLink="https://community.postman.com/"
+                />
           </div>
         </section>
       </Layout>
